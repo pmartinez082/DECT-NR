35450a86b77886aa9540320fb33b53ad3a993e1a
pmartine082 <pmartinez082@ikasle.ehu.eus>
2025-11-26 17:24:02 +0100

    more anite measurements

--- output_metrics/per_snr.py
+++ output_metrics/per_snr.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import csv
 
 # === Load CSV ===
 df = pd.read_csv('csv_files/anite_gauss.csv')
 
 # === Clean data ===
 df.columns = df.columns.str.lower()
+
 
 # Drop rows where snr is <= 0
 df = df[df['snr'] > 0]
@@ -15,12 +17,18 @@ df = df[df['snr'] > 0]
 # PDC → 0, PCC → 0, PDC_ERR/PCC_ERR → 1
 df['packet_error'] = df['channel'].apply(
     lambda x: 0 if x.upper() == 'PDC' or x.upper() == 'PCC'
-    else 1 if x.upper() in ['PDC_ERR']
+    else 1 if x.upper() in ['PDC_ERR' ]
     else np.nan
 )
 
 # Drop rows where packet_error is NaN (unexpected channel names)
 df = df.dropna(subset=['packet_error'])
+
+# Drop rows where mcs > 4 | mcs < 1
+df = df[df['mcs'] > 0]
+df = df[df['mcs'] < 5]
+
+
 
 # === Compute PER per SNR per MCS ===
 per_data = (
@@ -48,6 +56,29 @@ plt.yscale('log')  # <<< Make y-axis logarithmic
 plt.legend(title='MCS')
 plt.tight_layout()
 
+
+
+# Print snr values that need more data (1M samples needed per SNR value per MCS) and save to CSV file
+
+csv_data = [ ['SNR (dB)', 'MCS', 'Current Samples', 'Additional Samples Needed'] ]
+required_samples = 1_000_000
+for _, row in per_data.iterrows():
+    if int(df[(df['snr'] == row['snr']) & (df['mcs'] == row['mcs'])].shape[0]) < required_samples:
+        csv_data.append([row['snr'], int(row['mcs']), int(df[(df['snr'] == row['snr']) & (df['mcs'] == row['mcs'])].shape[0]), required_samples - int(df[(df['snr'] == row['snr']) & (df['mcs'] == row['mcs'])].shape[0])])
+
+# sort csv_data by MCS and SNR
+# Separate header and data, then sort data only
+csv_header = csv_data[0]
+csv_data_rows = csv_data[1:]
+csv_data_rows = sorted(csv_data_rows, key=lambda x: (int(x[1]), float(x[0])))
+csv_data = [csv_header] + csv_data_rows
+
 # Save and show
 plt.savefig('output/AWGN.pdf', format='pdf')
-plt.show()
+
+
+# Save CSV data
+with open('output/statistics_AWGN.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(csv_data)
+
